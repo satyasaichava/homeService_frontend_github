a4ba1a6f7b28d7a5aebae1b3b34c9beea76941e8
LostandFound0 <lostandfound095@gmail.com>
2025-03-25 09:59:18 -0400

    new work
    new work

--- myapp/vite.config.js
+++ myapp/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-app/',  // Add this line if you're deploying to a subpath
+  base: '/', // Adjust this if deploying to a subfolder, e.g., '/myapp/'
 })
